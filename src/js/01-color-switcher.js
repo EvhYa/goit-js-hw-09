ed380007d00738b928e3d6ec22a3288b16570e49
EvhYa <nicetea.bro@gmail.com>
2023-07-05 21:46:41 +0300

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+buttonStart.addEventListener('click', changeStart);
+buttonStop.addEventListener('click', changeStop);
+
+let intervalId;
+
+function changeStart() {
+  intervalId = setInterval(colorChange, 1000);
+  buttonStart.disabled = true;
+}
+
+function colorChange() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function changeStop() {
+  clearInterval(intervalId);
+  buttonStart.disabled = false;
+}
