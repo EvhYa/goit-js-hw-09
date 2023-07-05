25ba8ac7bca45a0ece8a96eb00667c83058bb774
EvhYa <125199323+EvhYa@users.noreply.github.com>
2023-07-05 18:47:12 +0000

    Deploying to gh-pages from @ EvhYa/goit-js-hw-09@ed380007d00738b928e3d6ec22a3288b16570e49 🚀

---
+++ 01-color-switcher.dccc0471.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),a=document.querySelector("body");function o(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.addEventListener("click",(function(){t=setInterval(o,1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.dccc0471.js.map
